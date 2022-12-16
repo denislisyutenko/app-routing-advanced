be1a31ea2946fbf191efe65d0a6fab3df8898865
Denis Lisyutenko <denislisyutenko@gmail.com>
2022-12-16 17:45:57 +0200

    first commit

---
+++ src/app/phrase-details/phrase-details.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {Phrase} from '../shared/phrase';
+import {PhrasesService} from '../shared/phrases.service';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-phrase-details',
+  templateUrl: './phrase-details.component.html',
+  styleUrls: ['./phrase-details.component.scss']
+})
+export class PhraseDetailsComponent implements OnInit {
+
+  phrase?: Phrase;
+
+  constructor(private phrasesService: PhrasesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.forEach(params => {
+      const id = +params['id'];
+
+      if (isNaN(id)) return;
+
+      this.phrasesService
+        .getPhrase(id)
+        .then(result => this.phrase = result);
+    });
+  }
+
+  gotoPhrasesList(): void {
+    this.router.navigate(['/phrases']).then();
+  }
+
+}
---
+++ src/app/shared/mock-data.ts
@@ -0,0 +1,13 @@
+import {Phrase} from './phrase';
+
+export const PHRASES = [
+  new Phrase(1, 'Привіт світ', 'Ukrainian'),
+  new Phrase(2, 'Hello World', 'English'),
+  new Phrase(3, 'Hola Mundo', 'Spanish'),
+  new Phrase(4, 'Bonjour le monde', 'France'),
+  new Phrase(5, 'Hello Welt', 'German'),
+  new Phrase(6, 'Ciao mondo', 'Italian'),
+  new Phrase(7, 'Witaj świecie', 'Polish'),
+  new Phrase(8, 'Hej världen', 'Swdish'),
+  new Phrase(9, 'Pozdravljen, svet', 'Slovenian'),
+];
